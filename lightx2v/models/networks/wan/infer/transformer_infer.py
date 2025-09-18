ff71c6d465804750a3c04e3e0870c0e1bdaa1a59
Toryn <1584340372@qq.com>
2025-09-19 03:14:11 +0800

    prompt1

--- lightx2v/models/networks/wan/infer/transformer_infer.py
+++ lightx2v/models/networks/wan/infer/transformer_infer.py
@@ -161,21 +161,21 @@ class WanTransformerInfer(BaseTransformerInfer):
         q = self.apply_rotary_emb_func(q, freqs_i)
         k = self.apply_rotary_emb_func(k, freqs_i)
 
-        # 0～11head:
-        for head in range(12):
-            q_head = q[:, head, :]  # [seq, head_dim]
-            k_head = k[:, head, :]  # [seq, head_dim]
-            attn = torch.matmul(q_head, k_head.T)  # [seq, seq]
-            plt.figure(figsize=(6, 5))
-            plt.imshow(attn.cpu().float(), aspect='auto', cmap='viridis')
-            plt.colorbar()
-            plt.title(f"Head {head}")
-            plt.tight_layout()
-            save_path = os.path.expanduser(
-                f"~/Log/09-17/Prompt1/head_{head}.png"
-            )
-            plt.savefig(save_path)
-            plt.close()
+        # # 0～11head:
+        # for head in range(12):
+        #     q_head = q[:, head, :]  # [seq, head_dim]
+        #     k_head = k[:, head, :]  # [seq, head_dim]
+        #     attn = torch.matmul(q_head, k_head.T)  # [seq, seq]
+        #     plt.figure(figsize=(6, 5))
+        #     plt.imshow(attn.cpu().float(), aspect='auto', cmap='viridis')
+        #     plt.colorbar()
+        #     plt.title(f"Head {head}")
+        #     plt.tight_layout()
+        #     save_path = os.path.expanduser(
+        #         f"~/Log/09-17/Prompt1/head_{head}.png"
+        #     )
+        #     plt.savefig(save_path)
+        #     plt.close()
 
         k_lens = torch.empty_like(seq_lens).fill_(freqs_i.size(0))
         cu_seqlens_q, cu_seqlens_k = self._calculate_q_k_len(q, k_lens=k_lens)
@@ -242,6 +242,23 @@ class WanTransformerInfer(BaseTransformerInfer):
             q,
             k_lens=torch.tensor([k.size(0)], dtype=torch.int32, device=k.device),
         )
+
+        # 0～11head:
+        for head in range(12):
+            q_head = q[:, head, :]  # [seq, head_dim]
+            k_head = k[:, head, :]  # [seq, head_dim]
+            attn = torch.matmul(q_head, k_head.T)  # [seq, seq]
+            plt.figure(figsize=(6, 5))
+            plt.imshow(attn.cpu().float(), aspect='auto', cmap='viridis')
+            plt.colorbar()
+            plt.title(f"Head {head}")
+            plt.tight_layout()
+            save_path = os.path.expanduser(
+                f"~/Log/09-19/Prompt1/head_{head}.png"
+            )
+            plt.savefig(save_path)
+            plt.close()
+
         attn_out = phase.cross_attn_1.apply(
             q=q,
             k=k,
