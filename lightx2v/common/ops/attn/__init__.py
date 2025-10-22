0fd39e8ff1838bc6f6ba445f3b881b0c8bc621f9
Toryn <1584340372@qq.com>
2025-10-22 20:00:05 +0800

    introduce spas_sage_attn

--- lightx2v/common/ops/attn/__init__.py
+++ lightx2v/common/ops/attn/__init__.py
@@ -1,7 +1,7 @@
 from .flash_attn import FlashAttn2Weight, FlashAttn3Weight
 from .radial_attn import RadialAttnWeight
 from .ring_attn import RingAttnWeight
-# from .sage_attn import SageAttn2Weight
+from .sage_attn import SageAttn2Weight
 from .svg2_attn import Svg2AttnWeight
 from .svg_attn import SvgAttnWeight
 from .torch_sdpa import TorchSDPAWeight
--- lightx2v/common/ops/attn/sage_attn.py
+++ lightx2v/common/ops/attn/sage_attn.py
@@ -1,50 +1,50 @@
-# import torch
-# from loguru import logger
+import torch
+from loguru import logger
 
-# from lightx2v.utils.registry_factory import ATTN_WEIGHT_REGISTER
+from lightx2v.utils.registry_factory import ATTN_WEIGHT_REGISTER
 
-# from .template import AttnWeightTemplate
+from .template import AttnWeightTemplate
 
-# if torch.cuda.get_device_capability(0) == (8, 9):
-#     try:
-#         from sageattention import sageattn_qk_int8_pv_fp16_triton as sageattn
-#     except ImportError:
-#         logger.info("sageattn not found, please install sageattention first")
-#         sageattn = None
-# else:
-#     try:
-#         from sageattention import sageattn
-#     except ImportError:
-#         logger.info("sageattn not found, please install sageattention first")
-#         sageattn = None
+if torch.cuda.get_device_capability(0) == (8, 9):
+    try:
+        from sageattention import sageattn_qk_int8_pv_fp16_triton as sageattn
+    except ImportError:
+        logger.info("sageattn not found, please install sageattention first")
+        sageattn = None
+else:
+    try:
+        from sageattention import sageattn
+    except ImportError:
+        logger.info("sageattn not found, please install sageattention first")
+        sageattn = None
 
 
-# @ATTN_WEIGHT_REGISTER("sage_attn2")
-# class SageAttn2Weight(AttnWeightTemplate):
-#     def __init__(self):
-#         self.config = {}
+@ATTN_WEIGHT_REGISTER("sage_attn2")
+class SageAttn2Weight(AttnWeightTemplate):
+    def __init__(self):
+        self.config = {}
 
-#     def apply(
-#         self,
-#         q,
-#         k,
-#         v,
-#         cu_seqlens_q=None,
-#         cu_seqlens_kv=None,
-#         max_seqlen_q=None,
-#         max_seqlen_kv=None,
-#         model_cls=None,
-#     ):
-#         q, k, v = q.contiguous(), k.contiguous(), v.contiguous()
-#         if len(q.shape) == 3:
-#             bs = 1
-#             q, k, v = q.unsqueeze(0), k.unsqueeze(0), v.unsqueeze(0)
-#         elif len(q.shape) == 4:
-#             bs = q.shape[0]
-#         x = sageattn(
-#             q,
-#             k,
-#             v,
-#             tensor_layout="NHD",
-#         ).view(bs * max_seqlen_q, -1)
-#         return x
+    def apply(
+        self,
+        q,
+        k,
+        v,
+        cu_seqlens_q=None,
+        cu_seqlens_kv=None,
+        max_seqlen_q=None,
+        max_seqlen_kv=None,
+        model_cls=None,
+    ):
+        q, k, v = q.contiguous(), k.contiguous(), v.contiguous()
+        if len(q.shape) == 3:
+            bs = 1
+            q, k, v = q.unsqueeze(0), k.unsqueeze(0), v.unsqueeze(0)
+        elif len(q.shape) == 4:
+            bs = q.shape[0]
+        x = sageattn(
+            q,
+            k,
+            v,
+            tensor_layout="NHD",
+        ).view(bs * max_seqlen_q, -1)
+        return x
