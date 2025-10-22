4dbd20d1d8142c6ef9ecef00627c79cf04d5b7ee
Toryn <1584340372@qq.com>
2025-10-22 15:03:07 +0800

    modify scripts

--- lightx2v/infer.py
+++ lightx2v/infer.py
@@ -20,14 +20,26 @@ from lightx2v.utils.set_config import print_config, set_config, set_parallel_con
 from lightx2v.utils.utils import seed_all
 
 
+######################################################################################################################################################
+# 1. 初始化runner
 def init_runner(config):
+    # 1. 关闭自动求导机制
+    # 1. 因为推理时用反向传播去更新参数，所以不需要记录自动求导
     torch.set_grad_enabled(False)
+
+    # 2.1 初始化runner，设置配置，设置设备GPU
     runner = RUNNER_REGISTER[config["model_cls"]](config)
+
+    # 2.2 初始化model
     runner.init_modules()
     return runner
 
 
+######################################################################################################################################################
+# 1. 推理入口
 def main():
+    ##################################################
+    # 1. 解析参数
     parser = argparse.ArgumentParser()
     parser.add_argument("--seed", type=int, default=42, help="The seed for random generator")
     parser.add_argument(
@@ -50,21 +62,16 @@ def main():
         ],
         default="wan2.1",
     )
-
     parser.add_argument("--task", type=str, choices=["t2v", "i2v", "t2i", "i2i", "flf2v", "vace", "animate", "s2v"], default="t2v")
     parser.add_argument("--model_path", type=str, required=True)
     parser.add_argument("--sf_model_path", type=str, required=False)
     parser.add_argument("--config_json", type=str, required=True)
     parser.add_argument("--use_prompt_enhancer", action="store_true")
-
     parser.add_argument("--prompt", type=str, default="", help="The input prompt for text-to-video generation")
     parser.add_argument("--negative_prompt", type=str, default="")
-
     parser.add_argument("--image_path", type=str, default="", help="The path to input image file for image-to-video (i2v) task")
     parser.add_argument("--last_frame_path", type=str, default="", help="The path to last frame file for first-last-frame-to-video (flf2v) task")
     parser.add_argument("--audio_path", type=str, default="", help="The path to input audio file or directory for audio-to-video (s2v) task")
-
-    # [Warning] For vace task, need refactor.
     parser.add_argument(
         "--src_ref_images",
         type=str,
@@ -83,29 +90,31 @@ def main():
         default=None,
         help="The file of the source mask. Default None.",
     )
-
     parser.add_argument("--save_result_path", type=str, default=None, help="The path to save video path/file")
     parser.add_argument("--return_result_tensor", action="store_true", help="Whether to return result tensor. (Useful for comfyui)")
     args = parser.parse_args()
-
     seed_all(args.seed)
 
-    # set config
+    # 2. 设置配置
     config = set_config(args)
 
+    # 3. 分布式推理
     if config["parallel"]:
         dist.init_process_group(backend="nccl")
         torch.cuda.set_device(dist.get_rank())
         set_parallel_config(config)
-
     print_config(config)
 
+    # 4. 开始推理
     with ProfilingContext4DebugL1("Total Cost"):
+        # 4.1 初始化runner
         runner = init_runner(config)
         input_info = set_input_info(args)
+
+        # 4.2 开始推理
         runner.run_pipeline(input_info)
 
-    # Clean up distributed process group
+    # 5. 分布式训练进程清理
     if dist.is_initialized():
         dist.destroy_process_group()
         logger.info("Distributed process group cleaned up")
--- lightx2v/utils/set_config.py
+++ lightx2v/utils/set_config.py
@@ -9,6 +9,8 @@ from lightx2v.utils.input_info import ALL_INPUT_INFO_KEYS
 from lightx2v.utils.lockable_dict import LockableDict
 
 
+############################################################################################################################################
+# 1. 获取默认属性
 def get_default_config():
     default_config = {
         "do_mm_calib": False,
@@ -32,16 +34,25 @@ def get_default_config():
     return default_config
 
 
+############################################################################################################################################
+# 1. 所有的属性都会被设置为一级属性，通过config.xxx取出
 def set_config(args):
+    ##################################################################################################################################
+    # 1.1 设置config的属性，一共来自: 默认属性，参数，本项目中的配置文件，模型目录下的配置文件
+    # 1.1 获取默认属性
     config = get_default_config()
+
+    # 1.2 根据args参数更新部分属性
     config.update({k: v for k, v in vars(args).items() if k not in ALL_INPUT_INFO_KEYS})
 
+    # 1.4 读取参数指定的配置文件，然后更新部分属性
     if config.get("config_json", None) is not None:
         logger.info(f"Loading some config from {config['config_json']}")
         with open(config["config_json"], "r") as f:
             config_json = json.load(f)
         config.update(config_json)
 
+    # 1.5 如果模型目录下也有config.json文件，读取，继续更新部分属性
     if os.path.exists(os.path.join(config["model_path"], "config.json")):
         with open(os.path.join(config["model_path"], "config.json"), "r") as f:
             model_config = json.load(f)
@@ -58,7 +69,9 @@ def set_config(args):
         with open(os.path.join(config["model_path"], "original", "config.json"), "r") as f:
             model_config = json.load(f)
         config.update(model_config)
-    # load quantized config
+    
+    # 1.6 如果本次推理使用的不是原模型，而是量化的模型
+    # 1.6 量化模型的目录下也有config.json文件，那就读取，继续更新部分属性
     if config.get("dit_quantized_ckpt", None) is not None:
         config_path = os.path.join(config["dit_quantized_ckpt"], "config.json")
         if os.path.exists(config_path):
@@ -66,11 +79,16 @@ def set_config(args):
                 model_config = json.load(f)
             config.update(model_config)
 
+    ##################################################################################################################################
+    # 2.1 target_video_length要求是4n+1，不满足的话，新找的4n+1是小于原来的target_video_length中最大的那个
+    # 2.1 这是由于默认配置中vae_stride是(4,8,8)，同时一般我们不在配置文件中指定vae_stride，所以要求target_video_length是4的倍数+1
     if config["task"] in ["i2v", "s2v"]:
         if config["target_video_length"] % config["vae_stride"][0] != 1:
             logger.warning(f"`num_frames - 1` has to be divisible by {config['vae_stride'][0]}. Rounding to the nearest number.")
             config["target_video_length"] = config["target_video_length"] // config["vae_stride"][0] * config["vae_stride"][0] + 1
 
+    ##################################################################################################################################
+    # 3.1 返回配置
     return config
 
 
