e51b9b3ddd4937f8f5c579197cba27348f06eeec
Toryn <1584340372@qq.com>
2025-10-22 16:16:07 +0800

    run spas_sage_attn

--- lightx2v/common/ops/attn/the_sage_attn.py
+++ lightx2v/common/ops/attn/the_sage_attn.py
@@ -1,12 +1,39 @@
 import spas_sage_attn
-import pkgutil
+import torch
+import matplotlib.pyplot as plt
+import os
 
-for module_info in pkgutil.iter_modules(spas_sage_attn.__path__):
-    print(module_info.name)
+# 1. 构造输入
+q = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
+k = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
+v = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
 
-# import spas_sage_attn
-# import torch
+# 2. 直接用PyTorch计算注意力
+q_ = q.float()
+k_ = k.float()
+v_ = v.float()
+attn_weights = torch.matmul(q_, k_.transpose(-2, -1)) / (128 ** 0.5)
+attn_weights = torch.softmax(attn_weights, dim=-1)
+output_pt = torch.matmul(attn_weights, v_)
 
-# q = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
-# k = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
-# v = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
+# 3. 用spas_sage2_attn_meansim_cuda计算注意力
+output_cuda = spas_sage_attn.core.spas_sage2_attn_meansim_cuda(q, k, v, tensor_layout="NHD")
+output_cuda = output_cuda.float()
+
+# 4. 取左上角[3000, 3000]，只取第一个head
+output_pt_crop = output_pt[0, 0, :3000, :3000].cpu().detach().numpy()
+output_cuda_crop = output_cuda[0, 0, :3000, :3000].cpu().detach().numpy()
+
+# 5. 保存图片
+save_dir = os.path.expanduser("~/Log/10-22/")
+os.makedirs(save_dir, exist_ok=True)
+
+plt.imshow(output_pt_crop, aspect='auto')
+plt.title('PyTorch Attention (left-top 3000x3000)')
+plt.savefig(os.path.join(save_dir, "attn.png"))
+plt.close()
+
+plt.imshow(output_cuda_crop, aspect='auto')
+plt.title('spas_sage2_attn_meansim_cuda (left-top 3000x3000)')
+plt.savefig(os.path.join(save_dir, "spas_attn.png"))
+plt.close()
