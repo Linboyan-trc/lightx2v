4a35dbc9f8a49bb909a5c6411c574cfc92f9214e
Toryn <1584340372@qq.com>
2025-10-22 21:48:49 +0800

    the_sage_attn

--- lightx2v/common/ops/attn/the_sage_attn.py
+++ lightx2v/common/ops/attn/the_sage_attn.py
@@ -22,8 +22,14 @@ class SageAttnWeight(AttnWeightTemplate):
         max_seqlen_q=None,
         max_seqlen_kv=None,
         model_cls=None,
-        tensor_layout="NHD"
+        tensor_layout="HND"
     ):
+        q = q.unsqueeze(0) 
+        k = k.unsqueeze(0)
+        v = v.unsqueeze(0)
+        q = q.transpose(1, 2)
+        k = k.transpose(1, 2)
+        v = v.transpose(1, 2)
         return spas_sage_attn.core.spas_sage2_attn_meansim_cuda(q, k, v, tensor_layout)
 
 if __name__ == "__main__":
