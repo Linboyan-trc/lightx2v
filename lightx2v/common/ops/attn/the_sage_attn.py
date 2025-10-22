170c266822c99ab3870c554eb9abb09fc53fa752
Toryn <1584340372@qq.com>
2025-10-22 15:58:02 +0800

    check modules in spas_sage_attn

--- lightx2v/common/ops/attn/the_sage_attn.py
+++ lightx2v/common/ops/attn/the_sage_attn.py
@@ -1,7 +1,5 @@
 import spas_sage_attn
-import pkgutil
-
-for module_info in pkgutil.iter_modules(spas_sage_attn.__path__):
+for module_info in pkgutil.iter_modules(the_sage_attn.__path__):
     print(module_info.name)
 
 # import spas_sage_attn
@@ -10,5 +8,3 @@ for module_info in pkgutil.iter_modules(spas_sage_attn.__path__):
 # q = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
 # k = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
 # v = torch.randn(32760, 12, 128, dtype=torch.bfloat16).cuda()
-
-
